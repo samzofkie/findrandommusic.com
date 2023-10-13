de5ac23643e018a754da0798ec0ebed171de6ef6
Sam <samzofkie@gmail.com>
2023-10-13 13:12:14 -0500

    Wrote Song component, custom stateful context provider PlaybackProvider to control audio playback globally, and did some general refactoring.

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,31 @@
-import React , { StrictMode, useState } from "react";
+import React , { StrictMode, useState, useRef, createContext, useContext} from "react";
 import { createRoot } from "react-dom/client";
 // import "./styles.css";
 
+
+function Song({songString=''}) {
+  if (songString) {
+    const audioRef = useRef();
+    const {pause, play} = useContext(PlaybackContext);
+    
+    const [artworkUrl, playbackUrl] = songString.split('@');
+ 
+    const audio = (
+      <audio ref={audioRef}>
+        <source src={playbackUrl} type={'audio/mpeg'} />
+      </audio>
+    );
+
+    const image = <img src={artworkUrl} onClick={() => {pause(); play(audioRef.current);}}/>;
+    
+    return (
+      <div className={'song'}>
+        {image}
+        {audio}
+      </div>
+    );
+  }
+}
 
 function DisplayRack({songs = []}) {
   if (!songs.length) 
@@ -9,10 +33,7 @@ function DisplayRack({songs = []}) {
 
   return (
     <div className={'display-rack'}>
-      {songs.map((song, i) => {
-        if (song)
-          return <img key={i} src={song.split('@')[0]} />
-      })}
+      {songs.map((songString, i) => <Song songString={songString} key={i}/>)}
     </div>
   );
 }
@@ -34,10 +55,41 @@ function App() {
   );
 }
 
+const PlaybackContext = createContext();
+
+function PlaybackProvider({ children }) {
+  const [currentlyPlaying, setCurrentlyPlaying] = useState('');
+
+  function pause() {
+    console.log('trying to pause:');
+    console.log(currentlyPlaying);
+    if (currentlyPlaying) {
+      console.log('in pause if.');
+      currentlyPlaying.pause()
+      setCurrentlyPlaying('');
+    }
+  }
+
+  function play(newAudio) {
+    console.log('trying to play:');
+    console.log(newAudio);
+    newAudio.play();
+    setCurrentlyPlaying(newAudio);
+  }
+
+  return (
+    <PlaybackContext.Provider value={{ pause, play }}>
+      {children}
+    </PlaybackContext.Provider>
+  );
+}
+
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <StrictMode>
-    <App />
+    <PlaybackProvider>
+      <App />
+    </PlaybackProvider>
   </StrictMode>
 );
